52e2ad38fc39873e7042a08752c8e758e0fa815c
Rumen Damyanov <dev@rumenx.com>
2025-11-24 20:41:08 +0200

    Get package version from directly from package.json

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
  * Main entry point for @rumenx/chatbot
  */
 
+import packageJson from '../package.json';
+
 // Core classes
 export { Chatbot } from './core/Chatbot';
 export { ConfigurationValidator } from './core/ConfigurationValidator';
@@ -11,5 +13,5 @@ export { ErrorHandler } from './core/ErrorHandler';
 export type * from './types';
 export type { ChatStreamChunk } from './types/ChatbotTypes';
 
-// Version info
-export const VERSION = '1.0.1';
+// Version info (dynamically imported from package.json)
+export const VERSION = packageJson.version;
